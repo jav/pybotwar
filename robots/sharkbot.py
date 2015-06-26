523898794c43ab0767f11301b0535e5981f479e8
Andreas Launila <andreas.launila@magine.com>
2015-06-26 16:06:49 +0200

    Don't explode close

--- robots/sharkbot.py
+++ robots/sharkbot.py
@@ -15,7 +15,7 @@ class TheRobot(Robot):
             else:
                 self.force_direction = -1
             self.torque(25)
-            self.turret(40 * self.turret_direction)
+            self.turret(70 * self.turret_direction)
             self.force(20 * self.force_direction)
         else:
             self.turret(0)
@@ -28,7 +28,11 @@ class TheRobot(Robot):
             self.act_next = tick + 100
             if self.sensors['HEAT'] < 100:
                 if self.sensors['LOADING'] == 0 and  self.sensors['HEAT'] < 80:
-                    self.fire(dist)
+                    self.turret_direction = -self.turret_direction
+                    if dist < 4:
+                        self.fire()
+                    else:
+                        self.fire(dist)
                     self.log("Heat: %s" % self.sensors['HEAT'])
             else:
                 self.act_next = 0
